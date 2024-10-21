571c8f5207c206793fc026602633a496f4a6c40c
anhunter06 <119953012+anhunter06@users.noreply.github.com>
2024-10-21 18:34:02 +0700

    Kiểm tra số Fibo trong khoảng [a,b]

---
+++ Fibonacci.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+/*
+    Author: An Hunter
+    Name: Nguyễn Tuấn An
+    Created at: 10/21/2024
+*/
+bool isPerfectSquare(int n) {
+    int i = sqrt(n);
+    if (i* i== n) return true;
+    return false;
+}
+bool isFibonacci(int n) {
+    return isPerfectSquare(5 * n * n +4) || isPerfectSquare(5 * n * n - 4);
+}
+// Kiểm tra từ a đến b có những số Fibonacci nào.
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    for (int i = a; i <= b;i++) {
+        if (isFibonacci(i)) {
+            printf("%d is Fibonanci\n", i);
+        } else {
+            printf("%d is not Fibonanci\n", i);
+        }
+    }
+}
